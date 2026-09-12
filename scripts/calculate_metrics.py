ca6535605b0a7f182b7cdb58c3bd920bcf826a7b
CharizardLyon <103219237+CharizardLyon@users.noreply.github.com>
2026-09-12 15:50:15 -0500

    Corrections

--- scripts/calculate_metrics.py
+++ scripts/calculate_metrics.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-ANIPOSE_CSV = "path/to/anipose_csv"
-PREDICTIONS_CSV = "path/to/predictions_csv"
+ANIPOSE_CSV = "path/to/pose-3d.csv"
+PREDICTIONS_CSV = "path/to/prediction_csv"
 
 JOINTS = [
     "WRIST",
@@ -12,24 +12,24 @@ JOINTS = [
     "THUMB_IP",
     "THUMB_TIP",
 
-    "INDEX_FINGER_CMC",
     "INDEX_FINGER_MCP",
-    "INDEX_FINGER_IP",
+    "INDEX_FINGER_PIP",
+    "INDEX_FINGER_DIP",
     "INDEX_FINGER_TIP",
 
-    "MIDDLE_FINGER_CMC",
     "MIDDLE_FINGER_MCP",
-    "MIDDLE_FINGER_IP",
+    "MIDDLE_FINGER_PIP",
+    "MIDDLE_FINGER_DIP",
     "MIDDLE_FINGER_TIP", 
 
-    "RING_FINGER_CMC",
     "RING_FINGER_MCP",
-    "RING_FINGER_IP",
+    "RING_FINGER_PIP",
+    "RING_FINGER_DIP",
     "RING_FINGER_TIP",
 
-    "PNIKY_CMC",
     "PINKY_MCP",
-    "PINKY_IP",
+    "PINKY_PIP",
+    "PINKY_DIP",
     "PINKY_TIP",
 ]
 
@@ -62,9 +62,9 @@ for joint in JOINTS:
     pred_y = f"{joint}_y_pred"
     pred_z = f"{joint}_z_pred"
 
-    gt_x = f"{joint})_x_gt"
-    gt_y = f"{joint})_y_gt"
-    gt_z = f"{joint})_z_gt"
+    gt_x = f"{joint}_x_gt"
+    gt_y = f"{joint}_y_gt"
+    gt_z = f"{joint}_z_gt"
 
     required_columns = [
         pred_x,
